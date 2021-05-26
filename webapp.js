8c26d788b37fadff0cbfc589f2be973ad46a7278
Keru-A <alice.s.dowle@gmail.com>
2021-05-26 14:18:47 +1200

    Create new folder system for node functionality

--- webapp.js
+++ webapp.js
@@ -1,9 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const path = require('path');
 
 app.set("view engine","ejs");
+
+app.use(express.static(__dirname + '/views'));
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+    res.render('assets/styles/')
+    res.render('assets/scripts/')
+  });
+  
+  // about page
+  app.get('/about', function(req, res) {
+    res.render('pages/about');
+  });
+
+  app.get('/contact', function(req, res) {
+    res.render('pages/contact');
+  });
+
+  app.get('/projects', function(req, res) {
+    res.render('pages/projects');
+  });
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
