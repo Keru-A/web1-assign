9ea64c8e671f53c5f2e6908d4c0d5af900845364
Keru-A <alice.s.dowle@gmail.com>
2021-06-16 13:44:38 +1200

    Fetch and display image from unsplash on home

---
+++ views/assets/scripts/scripts.js
@@ -0,0 +1,38 @@
+fetch ('https://api.unsplash.com/photos/aI3EBLvcyu4?client_id=I3qdnpZXWiIkcwPOmvVcZ9yWwdepIH36eEonzKTA3ug')
+    .then(res => {
+        return res.json();
+    })
+
+    .then(data => {
+            console.log(data.urls.raw);
+            let img = document.getElementById("homebackgroundimg");
+            img.src = (data.urls.raw);
+        })
+
+
+
+
+
+
+    // fetch("https://reqres.in/api/users?page=2")
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         data.data.forEach(u => {                
+    //             let li = document.createElement("li");
+    //             let container = document.createElement("div");
+
+    //             let nameDiv = document.createElement("div")
+    //             nameDiv.innerHTML = `${u.first_name} ${u.last_name}`
+
+    //             let emailDiv = document.createElement("div")
+    //             emailDiv.innerHTML = `${u.email}`
+    //             container.append(nameDiv,emailDiv);
+
+    //             let img = document.createElement("img");
+    //             img.src = u.avatar;
+
+    //             li.append(img,container);
+
+    //             document.querySelector("#cards").append(li);
+    //         })
+    //     });
