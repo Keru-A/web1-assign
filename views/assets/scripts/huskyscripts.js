b00ae1fca19ba74cb56cdbbf51b974c9d3c7c448
Keru-A <alice.s.dowle@gmail.com>
2021-06-25 15:59:09 +1200

    Add content to husky page

--- views/assets/scripts/huskyscripts.js
+++ views/assets/scripts/huskyscripts.js
@@ -10,6 +10,30 @@ fetch ('https://api.unsplash.com/photos/54jiFcnouyw?client_id=I3qdnpZXWiIkcwPOmv
             img.src = (data.urls.raw);
         })
 
+// //fetch image for wide husky image
+fetch ('https://api.unsplash.com/photos/oIuDXlOJSiE?client_id=I3qdnpZXWiIkcwPOmvVcZ9yWwdepIH36eEonzKTA3ug')
+    .then(res => {
+        return res.json();
+    })
+    
+    .then(data => {
+            console.log(data.urls.raw);
+            let img = document.getElementById("huskywideimg");
+            img.src = (data.urls.raw);
+        })
+
+// //fetch image for husky blurb
+fetch ('https://api.unsplash.com/photos/LmJ3n1MfrTI?client_id=I3qdnpZXWiIkcwPOmvVcZ9yWwdepIH36eEonzKTA3ug')
+    .then(res => {
+        return res.json();
+    })
+    
+    .then(data => {
+            console.log(data.urls.raw);
+            let img = document.getElementById("huskyblurbimg");
+            img.src = (data.urls.raw);
+        })
+
 topButton = document.getElementById("topbutton");
 
 function topFunction() {
--- views/assets/scripts/shepherdscripts.js
+++ views/assets/scripts/shepherdscripts.js
@@ -10,6 +10,31 @@ fetch ('https://api.unsplash.com/photos/tBY61CbBJg8?client_id=I3qdnpZXWiIkcwPOmv
             img.src = (data.urls.raw);
         })
 
+//fetch image for wide shepherd
+fetch ('https://api.unsplash.com/photos/lOLywaWn8VY?client_id=I3qdnpZXWiIkcwPOmvVcZ9yWwdepIH36eEonzKTA3ug')
+    .then(res => {
+        return res.json();
+    })
+    
+    .then(data => {
+            console.log(data.urls.raw);
+            let img = document.getElementById("shepherdwideimg");
+            img.src = (data.urls.raw);
+        })
+
+//fetch image for shepherd blurb
+fetch ('https://api.unsplash.com/photos/Y9XRyobtsBI?client_id=I3qdnpZXWiIkcwPOmvVcZ9yWwdepIH36eEonzKTA3ug')
+    .then(res => {
+        return res.json();
+    })
+    
+    .then(data => {
+            console.log(data.urls.raw);
+            let img = document.getElementById("shepherdblurbimg");
+            img.src = (data.urls.raw);
+        })
+
+//scroll to top function
 topButton = document.getElementById("topbutton");
 
 function topFunction() {
